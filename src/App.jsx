b023a079ff642723b8752b52d23f3602aacfd7b7
abhi2002 <abhishekdhole60@gmail.com>
2024-07-10 17:45:48 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import React from "react";
 import Layout from "./Layout/Layout";
-import SignUp from "./pages/Login/SignUp";
-import Card from "./pages/cardP";
-import ProductsData from "./pages/Products/ProductsData";
 
 const App = () => {
   return (
     <div>
       <Layout />
-      {/* <SignUp /> */}
-      {/* <Card /> */}
-      <ProductsData/>
     </div>
   );
 };
