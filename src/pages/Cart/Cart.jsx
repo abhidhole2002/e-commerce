660340071f2a849cb0dd081944f9d8297931bc20
abhi2002 <abhishekdhole60@gmail.com>
2024-07-11 00:42:29 +0530

    update

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { TbMoodEmptyFilled } from "react-icons/tb";
 import { useDispatch, useSelector } from "react-redux";
 import { removeFromCart } from "../../Reducers/Cart";
